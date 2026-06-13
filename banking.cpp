d7a58cbf9a1b7545d38e3e2f4255b9825a1a9bf6
nemavhola phuluso <165846903+PhulusoN@users.noreply.github.com>
2026-06-13 16:30:56 +0200

    Update banking.cpp

--- banking.cpp
+++ banking.cpp
@@ -1,41 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int Pin[5] = {2003, 2000, 1985, 1200, 8121};
-string Names[5] = {"Mike", "John", "Sarah", "Anna", "Tom"};
-int AccountNumber[5] = {12345, 67890, 54321, 98765, 13579};
-double Balance[5] = {1000.50, 2500.75, 500.25, 1500.00, 3000.00};
+void deposit(int* balance) {
+    int amount;
+    cout << "Enter deposit amount: ";
+    cin >> amount;                          
+    *balance = *balance + amount;           
+}
 
-int main()
-{
-    int userPin;
-    bool found = false; 
-
-    cout << "Welcome to World Bank\n";
-    cout << "Enter your PIN: ";
-    cin >> userPin;
-
-    for (int i = 0; i < 5; i++)
-    {
-        if (userPin == Pin[i])
-        {
-            found = true;
-
-            cout << "\nAccess Granted!\n";
-            cout << "-----------------------------\n";
-            cout << "Name:           " << Names[i] << "\n";
-            cout << "Account Number: " << AccountNumber[i] << "\n";
-            cout << "Balance:        R" << Balance[i] << "\n";
-            cout << "-----------------------------\n";
-
-            break; // ← stop searching once found
-        }
+void withdraw(int* balance) {
+    int amount;
+    cout << "Enter withdrawal amount: ";
+    cin >> amount;                          
+    if (amount > *balance) {               
+        cout << "Insufficient funds\n";
+    } else {
+        *balance = *balance - amount;
     }
+}
 
-    if (!found)
-    {
-        cout << "Access Denied — PIN not recognised.\n";
-    }
+int main() {
+    int balance = 5000;
+    cout << "--------------------------------------"<< "\n";
+    cout << "Current balance: R " << balance << "\n";
+
+    deposit(&balance);
+    cout << "Balance after deposit:    R" << balance << "\n";
+    cout << "-----------"<< "\n";
+
+    withdraw(&balance);
+    cout << "Balance after withdrawal: R " << balance << "\n";
+    cout << "-----------"<< "\n";
+
+    withdraw(&balance);
+    cout << "Balance after withdrawal: R " << balance << "\n";
+    cout << "--------------------------------------"<< "\n";
 
     return 0;
 }
